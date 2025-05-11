5d49af0dba60e87605ceba230be061951cad4221
Prateek Kumar Patel <153807112+PrateekK-2001@users.noreply.github.com>
2025-05-11 09:58:21 +0530

    Update MessageForm.jsx

--- FrontEnd/src/components/MessageForm.jsx
+++ FrontEnd/src/components/MessageForm.jsx
@@ -13,7 +13,7 @@ const MessageForm = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://new12-backend.onrender.com/api/v1/message/send",
         { firstName, lastName, email, phone, message },
         {
           withCredentials: true,
