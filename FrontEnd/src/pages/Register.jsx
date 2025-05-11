84d79f851a3ee1a993d30ced2366623260d7ff2b
Prateek Kumar Patel <153807112+PrateekK-2001@users.noreply.github.com>
2025-05-11 09:56:09 +0530

    Update Register.jsx

--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "https://new12-backend.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password ,role: "Patient"},// roles hata denge ager code eski vajah se dikket me hai to
           {
             withCredentials: true,
